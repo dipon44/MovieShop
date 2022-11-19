d683f6d61471340e8dfd84edb4fdb8215cf97231
dipon44 <dipon232@gmail.com>
2022-11-19 15:59:28 +0600

    Project Upload

---
+++ MovieFrontEnd/Movie_FrontEnd/src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { provideCloudflareLoader } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    this.moviesInTheather= [{
+      title: 'Spider-Man',
+      realeaseDate: new Date(),
+      price: 50.9
+    },
+    {
+      title: 'Bat-Man',
+      realeaseDate: new Date(),
+      price: 60.5
+    },
+    {
+      title: 'Super-Man',
+      realeaseDate: new Date(),
+      price: 70.9
+    }];
+
+
+    this.moviesFutureRelease=[{
+      title: 'Fast & Furious 7',
+      realeaseDate: new Date(),
+      price: 50.9
+    },
+    {
+      title: 'Fast & Furious 8',
+      realeaseDate: new Date(),
+      price: 60.5
+    },
+    {
+      title: 'Fast & furious 9',
+      realeaseDate: new Date(),
+      price: 70.9
+    }
+    ];
+  }
+  
+
+  moviesInTheather :any;
+  moviesFutureRelease :any;
+  title = "any value";
+
+  doubleNumber(n: number){
+    return n*2;
+  }
+
+  tripleNumber(m: number){
+    return m*3;
+  }
+}
---
+++ MovieFrontEnd/Movie_FrontEnd/src/app/movies/movie-list/movie-list.component.ts
@@ -0,0 +1,21 @@
+import { Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-list',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.css']
+})
+export class MovieListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  @Input()
+  movies: any;
+ // movies: { title: string; realeaseDate: Date; price: number; }[];
+
+}
